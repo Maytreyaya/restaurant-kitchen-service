b0818c843d2db25d8774d8a722c05ba8e3debc9c
Maksym Nikityuk <maksym71417@gmail.com>
2023-03-31 12:44:38 +0300

    new favicon added

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
 
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 # DEBUG = False
